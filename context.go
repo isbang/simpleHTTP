dcc5f632c561bc349e9f5608febdcfb49ec41cfd
isbang <forresti3979@gmail.com>
2020-08-23 23:38:57 +0900

    add context wrapper

--- context.go
+++ context.go
@@ -25,19 +25,71 @@ type Context interface {
 }
 
 func WithValue(ctx Context, key, val interface{}) Context {
-	panic("not implemented")
+	switch v := ctx.(type) {
+	case *simpleContext:
+		origin := v.req.Context()
+		c := context.WithValue(origin, key, val)
+		v.req = v.req.WithContext(c)
+		return v
+	case *contextWrapper:
+		c := context.WithValue(v.ctx, key, val)
+		v.ctx = c
+		return v
+	default:
+		c := context.WithValue(ctx, key, val)
+		return &contextWrapper{Context: ctx, ctx: c}
+	}
 }
 
 func WithCancel(ctx Context) (Context, CancelFunc) {
-	panic("not implemented")
+	switch v := ctx.(type) {
+	case *simpleContext:
+		origin := v.req.Context()
+		c, cancel := context.WithCancel(origin)
+		v.req = v.req.WithContext(c)
+		return v, cancel
+	case *contextWrapper:
+		c, cancel := context.WithCancel(v.ctx)
+		v.ctx = c
+		return v, cancel
+	default:
+		c, cancel := context.WithCancel(ctx)
+		return &contextWrapper{Context: ctx, ctx: c}, cancel
+	}
 }
 
 func WithTimeout(ctx Context, d time.Duration) (Context, CancelFunc) {
-	panic("not implemented")
+	switch v := ctx.(type) {
+	case *simpleContext:
+		origin := v.req.Context()
+		c, cancel := context.WithTimeout(origin, d)
+		v.req = v.req.WithContext(c)
+		return v, cancel
+	case *contextWrapper:
+		c, cancel := context.WithTimeout(v.ctx, d)
+		v.ctx = c
+		return v, cancel
+	default:
+		c, cancel := context.WithTimeout(ctx, d)
+		return &contextWrapper{Context: ctx, ctx: c}, cancel
+	}
 }
 
 func WithDeadline(ctx Context, t time.Time) (Context, CancelFunc) {
-	panic("not implemented")
+	switch v := ctx.(type) {
+	case *simpleContext:
+		origin := v.req.Context()
+		c, cancel := context.WithDeadline(origin, t)
+		v.req = v.req.WithContext(c)
+		return v, cancel
+	case *contextWrapper:
+		c, cancel := context.WithDeadline(v.ctx, t)
+		v.ctx = c
+		return v, cancel
+	default:
+		c, cancel := context.WithDeadline(ctx, t)
+		return &contextWrapper{Context: ctx, ctx: c}, cancel
+	}
 }
 
 type simpleContext struct {
@@ -113,4 +165,25 @@ func (c *simpleContext) AbortWithHtml(status int, html string) {
 func (c *simpleContext) AbortWithTemplate(status int, t *template.Template, obj interface{}) error {
 	c.Abort()
 	return c.Template(status, t, obj)
+}
+
+type contextWrapper struct {
+	Context
+	ctx context.Context
+}
+
+func (w *contextWrapper) Deadline() (deadline time.Time, ok bool) {
+	return w.ctx.Deadline()
+}
+
+func (w *contextWrapper) Done() <-chan struct{} {
+	return w.ctx.Done()
+}
+
+func (w *contextWrapper) Err() error {
+	return w.ctx.Err()
+}
+
+func (w *contextWrapper) Value(key interface{}) interface{} {
+	return w.ctx.Value(key)
 }
